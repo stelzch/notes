7a7b6e0abf9a098eec203ecb4a51f286f741f3c8
Christoph Stelz <mail@ch-st.de>
2016-09-22 17:00:48 +0200

    Improved Renderer Added syntax highlighting Added special syntax _box for boxes!

--- Renderer.py
+++ Renderer.py
@@ -4,11 +4,30 @@
 
 from Note import Note
 import mistune
-
-
+""" The Pygment Highlighter """
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import HtmlFormatter
 
 class NoteMarkdownRenderer(mistune.Renderer):
-    pass
+    def block_code(self, code, lang):
+        if not lang:
+            print("No language for "+code)
+            return '\n<pre><code>%s</code></pre>\n' % mistune.escape(code)
+        if lang == "_box":
+            lines = code.split("\n")
+            caption = lines[0]
+            content_ = lines[1:]
+            content = ""
+            for line in content_:
+                content += (line + "<br>")
+            return '\n<div class="box">\n<div class="caption">%s</div>\n%s\n</div>' % (caption, content)
+        print("Highlighting %s: %s" % (lang, code))
+        lexer = get_lexer_by_name(lang, stripall=True)
+        formatter = HtmlFormatter(full=True)
+        result = highlight(code, lexer, formatter)
+        print(result)
+        return highlight(code, lexer, formatter)
 
 class NoteRenderer:
     
